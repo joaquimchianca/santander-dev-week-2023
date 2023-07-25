289fc25e5c537f4bd29bb8fd5105d75994201af0
joaquimchianca <joaquimchianca@gmail.com>
2023-07-25 00:41:48 -0300

    feat: criacao dos modelos

---
+++ src/main/java/domain/model/Feature.java
@@ -0,0 +1,34 @@
+package domain.model;
+
+public class Feature {
+    private Long id;
+    private String icon;
+    private String description;
+
+    public Feature() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
