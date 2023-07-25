582c12b44607f2dbd68fc99fb80797d10599129a
joaquimchianca <joaquimchianca@gmail.com>
2023-07-25 05:03:40 -0300

    feat: User Controller

---
+++ src/main/java/me/dio/controller/UserController.java
@@ -0,0 +1,51 @@
+package me.dio.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/users")
+@Tag(name = "Users Controller", description = "RESTful API to manage users.")
+public record UserController(UserService userService) {
+
+    @GetMapping
+    public ResponseEntity<Page<User>> findAll(Pageable page) {
+        return ResponseEntity.ok(userService.listAll(page));
+    }
+
+    @GetMapping("/{id}}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        var userCreated = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        return ResponseEntity.ok(userService.update(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
